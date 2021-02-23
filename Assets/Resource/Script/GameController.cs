2533a0b994090fbbf1f0392ed543bddefcc6ca3f
DESKTOP-J6I4MVD\mrq <hongquang.mh@gmail.com>
2021-02-24 01:16:38 +0700

    no message

---
+++ Assets/Resource/Script/GameController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject bomb;
+    public Vector3 spawnValues;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnWave();
+    }
+
+    // Update is called once per frame
+    
+    void spawnWave()
+    {
+        Vector3 spawnPosition = new Vector3(Random.Range(-0.217f, 0.318f), spawnValues.y, -0.0094f);
+        Quaternion spawnRotation = new Quaternion();
+        Instantiate(bomb, spawnPosition, spawnRotation);
+
+
+    }
+}
---
+++ Assets/Resource/Script/Mover.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+   public float speed;
+    public Rigidbody rigid;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rigid.velocity = new Vector3(0, speed, 0);
+    }
+}
