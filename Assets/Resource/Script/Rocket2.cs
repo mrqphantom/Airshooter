e3931b48474ed6e3bbb436229be1c5731cbf8072
mrq1234 <hongquang.mh@gmail.com>
2021-03-02 18:32:23 +0700

    no message

--- Assets/Resource/Script/Rocket2.cs
+++ Assets/Resource/Script/Rocket2.cs
@@ -9,53 +9,56 @@ public class Rocket2 : MonoBehaviour
     public float speed;
     Vector3 angularVelocity;
     Rigidbody rigid;
-    public float turn;
+    public float minturn,maxturn;
     public GameObject Explosion;
+    float thisDistance;
+    public GameObject Dir;
+   
     // Start is called before the first frame update
     void Start()
     {
+
         
-     
     }
 
     // Update is called once per frame
     void Update()
     {
+        Dir= GameObject.FindGameObjectWithTag("Launcher");
         targets = GameObject.FindGameObjectsWithTag("Enemy");
         rigid = GetComponent<Rigidbody>();
-        
-       
-        GameObject closetTarget = targets[0];
-        float closetDistance = Vector3.Distance(transform.position, closetTarget.transform.position);
-        float thisDistance;
-        for(int i=1;i < targets.Length; i++)
+        if (targets.Length == 0)
         {
-            thisDistance = Vector3.Distance(transform.position, targets[i].transform.position);
-            if (thisDistance<closetDistance)
-            {
-                closettarget = targets[i];
-                closetDistance = thisDistance;
-                
 
+            
+            rigid.angularVelocity = transform.forward * speed;
+            return;
+            
+        }
+        if (targets.Length >= 1)
+        {
+            GameObject closetTarget = targets[0];
+            float closetDistance = Vector3.Distance(transform.position, closetTarget.transform.position);
+
+            for (int i = 1; i < targets.Length; i++)
+            {
+                thisDistance = Vector3.Distance(transform.position, targets[i].transform.position);
+                if (thisDistance < closetDistance)
+                {
+                    closettarget = targets[i];
+                    closetDistance = thisDistance;
+
+
+                }
             }
             var rocketRotation = Quaternion.LookRotation(closetTarget.transform.position - transform.position);
-            rigid.MoveRotation(Quaternion.RotateTowards(transform.rotation, rocketRotation, turn));
-           
+            rigid.MoveRotation(Quaternion.RotateTowards(transform.rotation, rocketRotation, Random.Range(minturn, maxturn)));
             rigid.velocity = transform.forward * speed;
 
 
 
         }
-        for(var i = targets.Length-1;i>=0;i--)
-        {
-            if(targets[i]==null)
 
-            {
-                targets[i]
-            }
-        }
-       
-
-        }
     }
-    
+}
+
--- Assets/Resource/Script/RocketLauncher.cs
+++ Assets/Resource/Script/RocketLauncher.cs
@@ -21,12 +21,8 @@ public class RocketLauncher : MonoBehaviour
 
         for (int i = 0; i < rocketCount; i++)
         {
-            float rotation = 0f;
-            if (i % 2 == 0)
-            { rotation = Random.Range(-140, -45); }
-            else
-            { rotation = Random.Range(45f, 140f); }
-            Instantiate(rocket, transform.position, Quaternion.Euler(0, rotation,0 ));
+            
+            Instantiate(rocket, transform.position, Quaternion.Euler(90f, 0,0 ));
             yield return new WaitForSeconds(WaiteachRocket);
 
         }
