2a6d8cb5e338d1eeb04903716e26458c2554d7a5
DESKTOP-J6I4MVD\mrq <hongquang.mh@gmail.com>
2021-03-05 02:39:24 +0700

    no message

---
+++ Assets/Resource/Script/AoEattack.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AoEattack : MonoBehaviour
+{
+    public float radius = 0.3f;
+    public GameObject explosion2;
+   
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     
+    }
+    void OnTriggerEnter(Collider other)
+    {if (other.tag == ("Enemy"))
+            {
+            Instantiate(explosion2, transform.position, transform.rotation);
+            Collider[] colliders = Physics.OverlapSphere(transform.position, radius);
+            foreach (Collider nearbyEnemy in colliders)
+            {
+                if (nearbyEnemy.tag == ("Enemy") && (nearbyEnemy != null))
+                {
+                    Instantiate(explosion2, nearbyEnemy.transform.position, transform.rotation);
+                    Debug.Log("Chamroi");
+                }
+            }
+            
+        }
+       
+
+    }
+   
+}
