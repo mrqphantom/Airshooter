e9a9feaf3b9f98447859016c83b5757ec017e317
DESKTOP-J6I4MVD\mrq <hongquang.mh@gmail.com>
2021-04-08 00:52:17 +0700

    no message

--- Assets/Resource/Script/GameController.cs
+++ Assets/Resource/Script/GameController.cs
@@ -22,12 +22,13 @@ public class GameController : MonoBehaviour
     public int points;
     public GameObject shield;
     GameObject shield_Obj;
-    float range=0.01f;
+    float range = 0.01f;
     PlayerShader playerShader;
     DestroybyContact destroybyContact;
     public GameObject health;
     public GameObject[] enemy;
-  
+    public GameObject SpeedUp;
+
 
     // Start is called before the first frame update
     void Start()
@@ -41,49 +42,54 @@ public class GameController : MonoBehaviour
             StartCoroutine(spawnWave());
             StartCoroutine(WaitGameStart());
             StartCoroutine(Dissolve());
+            StartCoroutine(spawnItem());
         }
-    
+
     }
-  
+
     void Update()
     {
         enemy = GameObject.FindGameObjectsWithTag("Enemy");
         if (!health)
-        {if(enemy.Length==0)
+        {
+            if (enemy.Length == 0)
             {
                 return;
             }
-          
-            {if (enemy.Length != 0)
+
+            {
+                if (enemy.Length != 0)
                 {
                     for (int i = 1; i < enemy.Length; i++)
                     {
-                      StartCoroutine( enemy[i].GetComponent<DestroybyContact>().Death(0f));  
+                        StartCoroutine(enemy[i].GetComponent<DestroybyContact>().Death(0f));
                     }
                     bombCount = 1;
                 }
             }
         }
         if (shield_Obj)
-        {if (health)
+        {
+            if (health)
             {
                 FindObjectOfType<HealthShader>().material.SetFloat("_Shield", 1);
                 FindObjectOfType<HealthShader>().material.SetFloat("_Hit", 0);
             }
         }
-        else if(!shield_Obj)
-        {if (health)
+        else if (!shield_Obj)
+        {
+            if (health)
             {
                 FindObjectOfType<HealthShader>().material.SetFloat("_Shield", 0);
             }
         }
     }
     // Update is called once per frame
-    
+
     IEnumerator spawnWave()
     {
         yield return new WaitForSeconds(startWave);
-        while(player != null)
+        while (player != null)
 
         {
             for (int i = 0; i < bombCount; i++)
@@ -101,32 +107,50 @@ public class GameController : MonoBehaviour
     }
     IEnumerator WaitGameStart()
     {
-      
+
         StartCoroutine(player.AddComponent<StartmoveAnimation>().MoveOverSpeed(StartPoint.position, StartSpeed));
         float t = 0;
-        while(t<timeStartGame)
+        while (t < timeStartGame)
         {
             t += Time.deltaTime;
             player.GetComponent<PlayerController>().enabled = false;
             obj = Instantiate(startParticle, player.transform.position, player.transform.rotation);
             obj.transform.parent = player.transform;
-            Destroy(obj,1f);
+            Destroy(obj, 1f);
             yield return new WaitForEndOfFrame();
-            
-            
+
+
         }
         player.GetComponent<PlayerController>().enabled = true;
-        Destroy(obj,0.5f);
+        Destroy(obj, 0.5f);
         player.GetComponent<StartmoveAnimation>().enabled = false;
     }
-  
+
     public IEnumerator Dissolve()
     {
-        for (int i=0;i<100;i++)
+        for (int i = 0; i < 100; i++)
         {
-            FindObjectOfType<PlayerShader>().material.SetFloat("_DissolveAmount", range+=0.01f);
+            FindObjectOfType<PlayerShader>().material.SetFloat("_DissolveAmount", range += 0.01f);
             yield return new WaitForSeconds(0.02f);
         }
     }
-  
+    IEnumerator spawnItem()
+    {
+        yield return new WaitForSeconds(2f);
+        while (player != null)
+
+        {
+            for (int i = 0; i < 1; i++)
+            {
+                Vector3 spawnPosition = new Vector3(Random.Range(-0.217f, 0.318f), spawnValues.y, -0.0094f);
+                Quaternion spawnRotation = new Quaternion();
+                Instantiate(SpeedUp, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(10f);
+            }
+            yield return new WaitForSeconds(5f);
+
+        }
+
+
+    }
 }
